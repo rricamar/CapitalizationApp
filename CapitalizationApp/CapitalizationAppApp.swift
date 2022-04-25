d724e8431a374cb0fb2f40054dcb8ffdc55d8b04
rricamar <r.ricamarti@gmail.com>
2022-04-25 17:00:44 +0200

    Initial Commit

---
+++ CapitalizationApp/CapitalizationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitalizationAppApp.swift
+//  CapitalizationApp
+//
+//  Created by Ruben Sopra on 25/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CapitalizationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
